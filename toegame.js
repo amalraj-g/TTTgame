36c6d7c36ed10b77f4223c49067d32497ebf5c35
amalraj-g <111746366+amalraj-g@users.noreply.github.com>
2022-12-06 21:11:03 +0530

    Add files via upload

--- toegame.js
+++ toegame.js
@@ -1,1 +1,95 @@
+const cells=document.querySelectorAll(".cell");
+const playStatus=document.querySelector("#status");
+const btnRestart=document.querySelector("#restart");
+let imageX="<img src='images/x.png'>";
+let imageO="<img src='images/o.png'>";
 
+const win=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+];
+
+let state=["","","","","","","","",""];
+let currentPlayer=imageX;
+let player="X";
+let running=false;
+const winningMessage = () => `Player ${player} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const PlayerTurn = () => `It's ${player}'s turn`;
+init();
+
+function init(){
+  cells.forEach(cell=>cell.addEventListener("click",cellClick));
+  btnRestart.addEventListener("click",restartGame);
+  playStatus.textContent=PlayerTurn();
+  running=true;
+}
+
+function cellClick(){
+  const index=this.dataset.index;
+  if(state[index]!="" || !running){
+    return;
+  }
+  updateBox(this,index);
+  checkWinner();
+}
+
+function updateBox(cell,index){
+    state[index]=player;
+    cell.innerHTML=currentPlayer;
+}
+
+function changePlayer(){
+    player=(player==="X") ? "O" :"X";
+    currentPlayer=(currentPlayer===imageX) ? imageO :imageX;
+    playStatus.textContent=PlayerTurn();
+}
+
+function checkWinner(){
+  let isWon=false;
+  for(let i=0;i<win.length;i++){
+    const condition=win[i];
+    const box1=state[condition[0]]; 
+    const box2=state[condition[1]]; 
+    const box3=state[condition[2]]; 
+    if(box1==="" || box2==="" || box3===""){
+      continue;
+    }
+    if(box1===box2 && box2===box3){
+      isWon=true;
+      cells[condition[0]].classList.add("win");
+      cells[condition[1]].classList.add("win");
+      cells[condition[2]].classList.add("win");
+    }
+  }
+
+  if(isWon){
+    playStatus.textContent = winningMessage();
+    running=false;
+  }else if(!state.includes("")){
+    playStatus.textContent= drawMessage();
+    running=false;
+  }else{
+    changePlayer();
+  }
+
+}
+
+function restartGame(){
+    state=["","","","","","","","",""];
+    currentPlayer=imageX;
+    player="X";
+    running=true;
+    playStatus.textContent=PlayerTurn();
+
+    cells.forEach(cell=>{
+        cell.innerHTML="";
+        cell.classList.remove("win");
+  });
+}
